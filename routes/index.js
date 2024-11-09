8916b0b10e9cf81c133f1252552a4ef9cd8d9911
sam-m03 <samrot.mur@gmail.com>
2024-11-08 21:53:14 -0500

    Update index.js
    Default page update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home' });
+});
 /* GET home page. */
 router.get('/home', function (req, res, next) {
   res.render('index', { title: 'Home' });
